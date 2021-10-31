911a1ca14386fa9e1d89ac8ec93c4dadb8ece544
md-farid <md.farid.ice@gmail.com>
2021-10-31 12:45:49 +0600

    break down into components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,30 @@
-import React from 'react';
-import data from './data';
+import React from "react";
+import Product from "./components/Product";
+import data from "./data";
 
 function App() {
   return (
     <div className="grid-container">
-    <header className="row">
-      <div>
-        <a className="brand" href="/">Shop</a>
-      </div>
-      <div>
-        <a href="/cart">Cart</a>
-        <a href="/login">Login</a>
-      </div>
-    </header>
-    <main>
-      <div className="row center">
-        {
-          data.products.map((product)=>(
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt="product"/>
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                </div>
-                <div className="price">
-                  ${product.price}
-                </div>
-          </div>
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Shop
+          </a>
         </div>
-          ))
-        }
-        
-      </div>
-    </main>
-    <footer className="row center">All right reversed</footer>
-  </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/login">Login</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All right reversed</footer>
+    </div>
   );
 }
 
